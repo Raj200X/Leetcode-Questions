cbb3f790bc24330c5ac52b942b6499df96dc75c6
Raj200X <91322992+Raj200X@users.noreply.github.com>
2025-11-02 22:29:36 +0530

    Time: 0 ms (100%), Space: 124.3 MB (92.86%) - LeetHub

--- 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.cpp
+++ 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     long long maxProduct(vector<int>& nums) {
-        long long product = 0;
-        vector<int> arr(2, 0); 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < 0) nums[i] = -nums[i];
+        long long max1 = 0, max2 = 0;
+        for (int n : nums) {
+            n = abs(n);
+            if (n > max1) {
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max2 = n;
+            }
         }
-        sort(nums.begin(), nums.end());
-        int cnt = 0;
-        arr[0] = nums[nums.size()-1];
-        arr[1] = nums[nums.size()-2];
-        product = 1LL * arr[0] * arr[1] * 100000;
-        return product; 
+        return 1LL * max1 * max2 * 100000;
     }
 };
