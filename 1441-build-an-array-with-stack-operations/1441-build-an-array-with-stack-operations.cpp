41eea2c7c13024bd9cb795d21dc5c3a4a450d936
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-01-26 13:16:03 +0530

    Time: 0 ms (100%), Space: 14.4 MB (5.73%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> ans;
-        int idx = 0;
+    bool isEqual(vector<int>& v, stack<int> s) {
+        if (v.size() != s.size()) return false;
 
-        for (int i = 1; i <= n && idx < target.size(); i++) {
-            ans.push_back("Push");
-            if (i == target[idx]) {
-                idx++;
+        for (int i = v.size() - 1; i >= 0; i--) {
+            if (s.empty() || v[i] != s.top())
+                return false;
+            s.pop();
+        }
+        return true;
+    }
+
+    vector<string> buildArray(vector<int>& target, int n) {
+        stack<int> s;
+        vector<string> ans;
+        unordered_set<int> st;
+
+        for (int x : target) st.insert(x);
+
+        for (int i = 1; i <= n; i++) {
+            if (st.count(i)) {
+                s.push(i);
+                ans.push_back("Push");
             } else {
+                s.push(i);
+                ans.push_back("Push");
+                s.pop();
                 ans.push_back("Pop");
             }
+
+            if (isEqual(target, s))
+                return ans;
         }
         return ans;
     }
