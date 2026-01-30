ff174ee93e1ea2ab697dc74e92ce67750916b7c8
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-01-30 13:37:12 +0530

    Time: 1260 ms (6.53%), Space: 119.1 MB (27.09%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int l = 0;
+        int h = k - 1;
+        int max_sum = INT_MIN;
+        vector<int> prefix_sum(nums.size(), 0);
+        prefix_sum[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i];
+        }
+        while (h < nums.size()) {
+            if (l>0) {
+                int sum = prefix_sum[h] - prefix_sum[l - 1];
+                cout << sum << " ";
+                max_sum = max(max_sum, sum);
+                l++;
+                h++;
+            }
+            else{
+                int sum = prefix_sum[h];
+            // cout << sum << " ";
+            max_sum = max(max_sum, sum);
+            l++;
+            h++;
+            }
+            
+        }
+        double ans = (double)max_sum / k;
+        return ans;
+    }
+};
