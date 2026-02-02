4a5d9f31534d2bfe13bbd8e551dc9f7048c6b127
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-02-02 13:41:25 +0530

    Time: 3 ms (94.97%), Space: 22.9 MB (76.83%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    double hours(vector<int>& piles, int mid){
-        double hs = 0;
-        for (int i=0;i<piles.size();i++){
-           hs += ceil((double)piles[i] / mid);
+    long long hours(vector<int>& piles, int k){
+        long long hs = 0;
+        for(int i = 0; i < piles.size(); i++){
+            hs += (piles[i] + k - 1) / k; 
         }
         return hs;
     }
-    int minEatingSpeed(vector<int>& piles, int hrs) {
-        int l = 1;
-        int max_ele = piles[0];
-        for (int i = 0;i<piles.size();i++){
-            max_ele = max(max_ele,piles[i]);
-        }
-        int h = max_ele;
-        while(l<=h){
-            int mid = (h+l)/2;
-            double hour = hours(piles,mid);
 
-            if (hour<=hrs){
-                h = mid-1;
-            }
-            else{
-                l = mid+1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            long long need = hours(piles, mid);
+
+            if(need <= h){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return l;
