909ed764e53db50f1a77dbe3b00c8a6cbcd08655
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-02-10 10:37:24 +0530

    Time: 27 ms (57.46%), Space: 61.1 MB (59.28%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,20 +1,50 @@
+class Pair{
+    public:
+    int a;
+    int b;
+    Pair(int a,int b){
+        this->a=a;
+        this->b=b;
+    }
+};
 class MyHashMap {
 public:
-    vector<int> arr;
+    vector<vector<Pair>> hm;
     MyHashMap() {
-        arr.resize(1000001, -1);
+    hm.resize(2069);
     }
     
-    void put(int key, int value) {  
-        arr[key] = value;
+    void put(int key, int value) {
+        int z=key%2069;
+        int flag=0;
+        for(int i=0;i<hm[z].size();i++){
+            if(hm[z][i].a==key){
+                flag=1;
+                hm[z][i].b=value;
+            }
+        }
+        if(flag==0){
+            hm[z].push_back(Pair(key,value));
+        }
     }
     
     int get(int key) {
-        return arr[key];
+        int z=key%2069;
+        for(int i=0;i<hm[z].size();i++){
+            if(hm[z][i].a==key){
+                return hm[z][i].b;
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
-        arr[key] = -1;
+        int z=key%2069;
+        for(int i=0;i<hm[z].size();i++){
+            if(hm[z][i].a==key){
+                hm[z].erase(hm[z].begin() + i);
+            }
+        }
     }
 };
 
