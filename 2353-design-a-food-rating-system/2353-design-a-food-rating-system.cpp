ab31af48619e8be4f6312d7e3ad60de067d43b01
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-02-23 15:44:39 +0530

    Time: 102 ms (93.91%), Space: 160.2 MB (95.99%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -8,26 +8,21 @@ public:
         }
     };
     unordered_map<string, pair<int,string>> foodInfo;
-
     unordered_map<string, priority_queue<pair<int,string>, vector<pair<int,string>>, comp>> cuisineMap;
-
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         int n = foods.size();
         for(int i = 0; i < n; i++) {
             foodInfo[foods[i]] = {ratings[i], cuisines[i]};
             cuisineMap[cuisines[i]].push({ratings[i], foods[i]});
         }
-    }
-    
+    } 
     void changeRating(string food, int newRating) {
         string cuisine = foodInfo[food].second;
         foodInfo[food].first = newRating;
         cuisineMap[cuisine].push({newRating, food});
     }
-    
     string highestRated(string cuisine) {
         auto &pq = cuisineMap[cuisine];
-
         while(true) {
             auto top = pq.top();
             if(foodInfo[top.second].first == top.first)
