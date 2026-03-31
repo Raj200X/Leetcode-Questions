5f949fda5c0bc2dc9eded0844afc5630a5323cf7
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-03-31 11:38:09 +0530

    Time: 0 ms (100%), Space: 12.1 MB (21.02%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,15 +11,38 @@
  */
 class Solution {
 public:
-    int sum = 0;
-    void inorder(TreeNode* root,int currNum){
-        if (root==nullptr) return;
-        inorder(root->left,currNum*10+root->val);
-        if (root->left==nullptr && root->right==nullptr) sum+=currNum*10+root->val;
-        inorder(root->right,currNum*10+root->val);
-    }
+    // int sum = 0;
+    // void inorder(TreeNode* root,int currNum){
+    //     if (root==nullptr) return;
+    //     inorder(root->left,currNum*10+root->val);
+    //     if (root->left==nullptr && root->right==nullptr) sum+=currNum*10+root->val;
+    //     inorder(root->right,currNum*10+root->val);
+    // }
     int sumNumbers(TreeNode* root) {
-        inorder(root,0);
+        // inorder(root,0);
+        // return sum;
+        queue<TreeNode*>q;
+        q.push(root);
+        queue<int>nums;
+        nums.push(root->val);
+        int sum =0;
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            q.pop();
+            int currSum = nums.front();
+            nums.pop();
+            if (temp->left) {
+                q.push(temp->left);
+                nums.push(currSum*10+temp->left->val);
+            }
+            if (temp->right) {
+                q.push(temp->right);
+                nums.push(currSum*10+temp->right->val);
+            }
+            if (temp->left==nullptr && temp->right==nullptr){
+                sum+=currSum;
+            }
+        }
         return sum;
 
     }
