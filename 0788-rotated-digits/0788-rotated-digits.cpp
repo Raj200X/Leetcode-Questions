0a4d0717fbded014b38e97648c1e60004079ad9c
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-05-02 14:59:13 +0530

    Time: 1 ms (44.56%), Space: 7.8 MB (83.68%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int temp = i;
+            bool valid = true;
+            bool changed = false;
+
+            while (temp > 0) {
+                int d = temp % 10;
+
+                if (d == 3 || d == 4 || d == 7) {
+                    valid = false;
+                    break;
+                }
+
+                if (d == 2 || d == 5 || d == 6 || d == 9) {
+                    changed = true;
+                }
+
+                temp /= 10;
+            }
+
+            if (valid && changed) ans++;
+        }
+
+        return ans;
+    }
+};
