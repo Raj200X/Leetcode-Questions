f0332e3868f3a0fde34eb47d99e914776ae3cfaf
Raj200X <91322992+Raj200X@users.noreply.github.com>
2026-05-08 11:27:40 +0530

    Time: 0 ms (100%), Space: 26.1 MB (54.43%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+
         int n = height.size();
-        stack <int> lmax;
-        stack <int> rmax;
-        lmax.push(0);
-        rmax.push(0);
-        int ans =0 ;
-        for (int i = 0;i<height.size();i++){
-            if (lmax.top()<height[i]){
-                lmax.push(height[i]);
+
+        int left = 0;
+        int right = n - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] <= height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                }
+                else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
             }
             else {
-                lmax.push(lmax.top());
-            }
-        }
-        for (int i = n-1;i>0;i--){
-            int ele = min(lmax.top(),rmax.top())-height[i];
-            if (ele>0){
-                ans = ans + ele;
-            }
-            if (rmax.top()<height[i]){
-                rmax.push(height[i]);
-            }
-            else{
-                rmax.push(rmax.top());
-            }
-            lmax.pop();
-        }
-        return ans;
 
-        
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                }
+                else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
     }
 };
